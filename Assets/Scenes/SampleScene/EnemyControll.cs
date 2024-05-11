86d8c835457c5d0a1ae0364eaeb726d3c173366e
Michał Mrowiec <michalmro210101@gmail.com>
2024-05-11 15:46:56 +0200

    Added shoot by enemy if see player

--- Assets/Scenes/SampleScene/EnemyControll.cs
+++ Assets/Scenes/SampleScene/EnemyControll.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyControll : MonoBehaviour
 {
     public int health = 1;
+    public Weapon weapon;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +16,12 @@ public class EnemyControll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        FieldOfView fov = gameObject.GetComponent<FieldOfView>();
+        if (fov.CanSeePlayer)
+        {
+            weapon.Fire();
+        }
+
     }
 
     public void TakeDamage(int damage = 1)
--- Assets/Scenes/SampleScene/Weapon.cs
+++ Assets/Scenes/SampleScene/Weapon.cs
@@ -7,10 +7,18 @@ public class Weapon : MonoBehaviour
     public GameObject bullet;
     public Transform firePoint;
     public float fireForce;
+    public float fireRate = 1;
+    public float nextFireTime = 0;
 
     public void Fire()
     {
-        GameObject projectile = Instantiate(bullet, firePoint.position, firePoint.rotation);
-        projectile.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
+        if(Time.time > nextFireTime)
+        {
+            GameObject projectile = Instantiate(bullet, firePoint.position, firePoint.rotation);
+            projectile.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
+
+            nextFireTime = Time.time + fireRate;
+        }
+
     }
 }
